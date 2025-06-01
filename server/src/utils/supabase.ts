12bc344ae594581c5f1a518d941ac23b9541570c
ameliazsabrina <152615423+ameliazsabrina@users.noreply.github.com>
2025-06-01 19:26:43 +0700

    fix: add missing env on server code

--- server/src/utils/supabase.ts
+++ server/src/utils/supabase.ts
@@ -3,7 +3,8 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseUrl =
+  process.env.SUPABASE_URL! || "https://fdigdwsrqrdzpwlnylog.supabase.co";
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
